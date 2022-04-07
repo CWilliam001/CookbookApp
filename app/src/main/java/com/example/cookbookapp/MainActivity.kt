d20339cf114a73587d9a12ae9f185367d1f35fa3
WilliamC <wlm011001@gmail.com>
2022-04-07 18:25:02 +0800

    Modify MainActivity.kt

--- app/src/main/java/com/example/cookbookapp/MainActivity.kt
+++ app/src/main/java/com/example/cookbookapp/MainActivity.kt
@@ -5,15 +5,19 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.cookbookapp.account.CreateAccount
 import com.example.cookbookapp.databinding.ActivityMainBinding
+import com.google.firebase.firestore.FirebaseFirestore
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var db : FirebaseFirestore
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        db = FirebaseFirestore.getInstance()
 
         binding.textViewCreateNewAccount.setOnClickListener{
             intent = Intent(this, CreateAccount::class.java)
