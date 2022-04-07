522662779020a29f84d53049e52a48df43a2e12a
WilliamC <wlm011001@gmail.com>
2022-04-07 20:32:27 +0800

    Add more colours set

--- app/src/main/java/com/example/cookbookapp/account/ViewProfile.kt
+++ app/src/main/java/com/example/cookbookapp/account/ViewProfile.kt
@@ -4,13 +4,23 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.cookbookapp.R
 import com.example.cookbookapp.databinding.CreateAccountBinding
+import com.google.firebase.firestore.FirebaseFirestore
 
 class ViewProfile : AppCompatActivity() {
 
     private lateinit var binding: CreateAccountBinding
+    private lateinit var db: FirebaseFirestore
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = CreateAccountBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        db = FirebaseFirestore.getInstance()
+
+        db.collection("User").whereEqualTo("id", intent.getStringExtra("id")).get().addOnSuccessListener {
+            //binding.
+        }
+
     }
 }
