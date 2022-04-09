c8206584df2db1d58c9ef08568dc2da15bd97619
Yoke Koon Soh <sohyk-pm19@student.tarc.edu.my>
2022-04-09 19:49:59 +0800

    - Update Dashboard.kt

--- app/src/main/java/com/example/cookbookapp/Dashboard.kt
+++ app/src/main/java/com/example/cookbookapp/Dashboard.kt
@@ -4,7 +4,11 @@ import android.content.Intent
 import android.os.Binder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.cookbookapp.account.Favourite
+import com.example.cookbookapp.account.History
 import com.example.cookbookapp.databinding.DashboardBinding
+import com.example.cookbookapp.model.Recipe
+import com.example.cookbookapp.model.Tag
 
 class Dashboard : AppCompatActivity() {
 
@@ -17,36 +21,42 @@ class Dashboard : AppCompatActivity() {
 
         // Binding buttons
         binding.buttonToSearch.setOnClickListener{
-            // intent = Intent(this, AddRecipe::class.java)
-            // startActivity(intent)
-            // finish()
+            intent = Intent(this, Recipe::class.java)
+            startActivity(intent)
+            finish()
         }
 
         binding.buttonToAddRecipe.setOnClickListener{
-            // intent = Intent(this, AddRecipe::class.java)
-            // startActivity(intent)
-            // finish()
+            intent = Intent(this, Recipe::class.java)
+            startActivity(intent)
+            finish()
         }
 
         binding.buttonToMyRecipe.setOnClickListener{
-            // intent = Intent(this, AddRecipe::class.java)
-            // startActivity(intent)
-            // finish()
+            intent = Intent(this, Recipe::class.java)
+            startActivity(intent)
+            finish()
         }
 
         binding.buttonToTags.setOnClickListener{
-            // intent = Intent(this, AddRecipe::class.java)
-            // startActivity(intent)
-            // finish()
+            intent = Intent(this, Tag::class.java)
+            startActivity(intent)
+            finish()
         }
 
         binding.buttonToFavourites.setOnClickListener{
-            // intent = Intent(this, AddRecipe::class.java)
-            // startActivity(intent)
-            // finish()
+            intent = Intent(this, Favourite::class.java)
+            startActivity(intent)
+            finish()
         }
 
         binding.buttonToHistory.setOnClickListener{
+            intent = Intent(this, History::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        binding.buttonToLogout.setOnClickListener{
             // intent = Intent(this, AddRecipe::class.java)
             // startActivity(intent)
             // finish()
