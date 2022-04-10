eaba33cf3e8d3f4a378edfc89cdc9599bfc1180c
weichung chew <weichung0251@gmail.com>
2022-04-10 11:18:29 +0800

    create account

--- app/src/main/java/com/example/cookbookapp/account/CreateAccount.kt
+++ app/src/main/java/com/example/cookbookapp/account/CreateAccount.kt
@@ -2,6 +2,7 @@ package com.example.cookbookapp.account
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.cookbookapp.databinding.CreateAccountBinding
 
 class CreateAccount : AppCompatActivity() {
@@ -12,5 +13,16 @@ class CreateAccount : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = CreateAccountBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        var editText_loginEmail = findViewById(binding.editText_loginEmail) as EditText
+        var editText_loginPassword = findViewById(binding.editText_loginPassword) as EditText
+        var btn_create = findViewById(binding.id.btn_reset) as Button
+        var btn_login = findViewById(binding.id.button_login) as Button
+
+        // set on-click listener
+        button_login.setOnClickListener {
+            val email = editText_loginEmail.text;
+            val password = editText_loginPassword.text;
+            Toast.makeText(this@CreateAccount, email, Toast.LENGTH_LONG).show()
     }
 }
